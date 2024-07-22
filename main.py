7761ee7d6c6e9dafb9045253408b2b303fa71c27
Ali AKBIYIK <aliiakbiyikk@gmail.com>
2024-07-22 22:30:56 +0300

    Veri ön işleme tamamlandı.

---
+++ main.py
@@ -0,0 +1,33 @@
+
+# Hikaye:
+# Bu veri seti, ünlü Iris veri setidir ve 1936'da Ronald A. Fisher tarafından tanıtılmıştır. 
+# Veri seti, üç farklı iris çiçeği türüne (Iris-setosa, Iris-versicolor, ve Iris-virginica) 
+# ait sepal uzunluğu, sepal genişliği, petal uzunluğu ve petal genişliği ölçümlerini içermektedir. 
+# Her bir iris türünden 50 örnek bulunmaktadır, toplamda 150 gözlem vardır.
+
+# Problem Tanımı:
+# Problem: İris Çiçeği Türünün Tahmin Edilmesi
+# Amaç, verilen sepal ve petal ölçümleri kullanılarak iris çiçeği türünü tahmin eden bir model geliştirmektir. 
+# Bu problem, bir sınıflandırma problemidir ve iris türlerini doğru bir şekilde sınıflandırmayı hedefler.
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+#Veri setini yükleme
+iris=pd.read_csv('Iris.csv')
+
+#İlk 5 değeri görüntüleme
+print(iris.head())
+
+#Eksik verileri kontrol etme
+print(iris.isnull().sum())
+
+#Veriyi standartlaştırma
+scaler = StandardScaler()
+iris[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']] = scaler.fit_transform(iris[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']])
+print(iris.head())
+
+#Kategorik verileri sayısal değere dönüştürme
+le = LabelEncoder()
+iris['Species'] = le.fit_transform(iris['Species'])
+print(iris.head())
