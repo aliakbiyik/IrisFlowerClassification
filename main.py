20321c57a2888cc2b4170a76aed0cd7a26678e27
Ali AKBIYIK <aliiakbiyikk@gmail.com>
2024-07-23 22:20:19 +0300

    Veri görselleştirmeye başlandı.

--- main.py
+++ main.py
@@ -11,6 +11,8 @@
 # Bu problem, bir sınıflandırma problemidir ve iris türlerini doğru bir şekilde sınıflandırmayı hedefler.
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 #Veri setini yükleme
@@ -18,6 +20,8 @@ iris=pd.read_csv('Iris.csv')
 
 #İlk 5 değeri görüntüleme
 print(iris.head())
+
+print(iris.describe())
 
 #Eksik verileri kontrol etme
 print(iris.isnull().sum())
@@ -31,3 +35,19 @@ print(iris.head())
 le = LabelEncoder()
 iris['Species'] = le.fit_transform(iris['Species'])
 print(iris.head())
+
+#Histograms
+numeric_columns=['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
+iris[numeric_columns].hist(bins=20,figsize=(12,8),layout=(2,2))
+plt.suptitle('Histogram of Features')
+plt.show()
+
+#Scatter plot
+sb.scatterplot(x='SepalLengthCm',y='PetalLengthCm',hue='Species',data=iris,palette='viridis' )
+plt.suptitle("Scatter Plot of Sepal Length vs. Petal Length")
+plt.show()
+
+#Pair plot
+sb.pairplot(iris,hue='Species',palette='viridis')
+plt.suptitle("Pair Plot of Iris Dataset",y=1.02)
+plt.show()
